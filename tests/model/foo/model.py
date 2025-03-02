eedfa08d47e0f5575edb850e3cd54705c33eb676
pyizi <pyizi@proton.me>
2025-03-02 11:34:23 +0300

    adding inheritance's tests

--- tests/model/foo/model.py
+++ tests/model/foo/model.py
@@ -1,6 +1,27 @@
 from tests.model.foo.bar.model_too import Type5, Type6
 
 
+class BaseForType3_2:
+    Z: str = "abc"
+
+    def __init__(self):
+        self.o = 321
+
+
+class BaseForBaseForType3:
+    def __init__(self, tui=43):
+        super().__init__()
+        self._tui = tui
+
+
+class BaseForType3_1(BaseForBaseForType3):
+    X: int = 12
+
+    def __init__(self, y):
+        super().__init__()
+        self.y = y
+
+
 class Type4:
     def __init__(self):
         self.x: int = 7
@@ -10,9 +31,10 @@ class Type4:
         self.lst: list = list(range(3))
 
 
-class Type3:
+class Type3(BaseForType3_1, BaseForType3_2):
     type6: Type6
 
     def __init__(self):
+        super().__init__(45)
         self.type5 = Type5()
         self.__dct = {Type5(): "3", Type6(7): Type4()}
