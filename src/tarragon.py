3b03893e5122579a09640e7e1a65b0314b8f307c
pyizi <pyizi@proton.me>
2025-03-04 19:20:40 +0300

    adding supporting binary formats + frozenset

--- src/tarragon.py
+++ src/tarragon.py
@@ -1,5 +1,9 @@
+import base64
 import json
+import types
 from typing import Any
+
+import dill
 
 
 class __DictRepresentor:
@@ -39,7 +43,7 @@ class __DictRepresentor:
 
             self.__real_to_virtual_id[real_obj_id] = virtual_obj_id
 
-            if obj_type in [list, tuple, set]:
+            if obj_type in [list, tuple, set, frozenset]:
                 array = []
                 for item in obj:
                     array.append(self.__get_representation(item))
@@ -61,15 +65,50 @@ class __DictRepresentor:
                     "items": items
                 }
             else:
-                # If custom class
-                fields = {}
-                for key, value in vars(obj).items():
-                    fields[key] = self.__get_representation(value)
-                return {
+                result = {
                     "type": obj_type.__module__ + "." + obj_type.__name__,
                     "id": virtual_obj_id,
-                    "object": fields
                 }
+
+                if hasattr(obj, "__dict__") and obj_type != types.FunctionType and not isinstance(obj, BaseException):
+                    fields = {}
+                    for key, value in vars(obj).items():
+                        fields[key] = self.__get_representation(value)
+                    result["object"] = fields
+                else:
+                    # Binarization:
+                    try:
+                        memoryview(obj)
+                    except TypeError:
+                        # If the type DOES NOT implement the buffer protocol, it must first be processed through dill
+                        result["base64"] = base64.b64encode(dill.dumps(obj)).decode("ascii")
+
+                        if obj_type is types.FunctionType and obj.__closure__:
+                            # TODO Functions with closures and without closures need to be distinguished.
+                            #  Functions without closures can be stored as they are, just in binary form.
+                            #  Functions with closures need to be stored along with the information about the values of
+                            #  the variables on which the closures are bound, including their "virtual_obj_id" and the
+                            #  full path to them in the program code. The thing is, after deserialization, a copy of the
+                            #  original state is created. For example, this will result in the scenario where if I had
+                            #  two functions bound to the same variable, after restoration this information would be
+                            #  lost; there would effectively be two separate variables and their states would be split.
+                            #  In classic dill, this is solved by serializing several functions together as a single
+                            #  unit, as part of one object. However, in my case everything is broken down into pieces
+                            #  and the state must be restored manually. Moreover, even dill does not know what to do
+                            #  with problems such as restoring a function whose state should be closed over a global
+                            #  variable, for example, or over any variable that was not included in the serialized
+                            #  object. I want, upon restoration, to search at the restoration point for the original
+                            #  variable on which everything was bound and re-bind the closure to it. And only if that
+                            #  variable does not exist in the current scope, then use its copy.
+                            #  Well, or this can be done optionally. Most likely, this will result in even primitives
+                            #  like "int" having to be stored with a "virtual_obj_id" (along with the type indication).
+                            #  This will make the format less readable.
+                            pass
+                    else:
+                        # If the type implements the buffer protocol, then it can be encoded to base64 directly
+                        result["base64"] = base64.b64encode(obj).decode("ascii")
+
+                return result
 
 
 def to_json(obj: Any) -> str:
--- tests/to_json/test_tarragon.py
+++ tests/to_json/test_tarragon.py
@@ -1,6 +1,8 @@
+import base64
 import json
 
 import assertpy
+import dill
 
 import tarragon
 from tests.model.foo.bar.model_too import Type5, Type6
@@ -143,35 +145,22 @@ def test_tuple_to_json():
     assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
 
 
-# FIXME тут видимо будут проблемы из-за неупорядоченности set
-def test_set_to_json():
-    type5_object = Type5(None, None, None, None, None)
-    type4_object = Type4(None, None, None, type5_object, None)
-    type5_object.type4 = type4_object
-    st = {type5_object, type4_object}
-    hook = Hook(st)
-    st.add(hook)
-    actual_json = tarragon.to_json(st)
+def test_frozenset_to_json():
+    hook = Hook()
+    st = frozenset([hook])
+    hook.ref = st
 
-    next_id_generator = next_id()
-    type5_representation = Type5Representation(None, None, None, None, None,
-                                               next_id_generator)
-    type4_representation = Type4Representation(None, None, None, type5_representation, None,
-                                               next_id_generator)
-    type5_representation._Type5Representation__type4_representation = type4_representation
-    current_id = next(next_id_generator)
+    actual_json = tarragon.to_json(st)
     expected_json = f"""{{
-        "type": "set",
-        "id": {current_id},
+        "type": "frozenset",
+        "id": 0,
         "array": [
-            {type5_representation()}, 
-            {type4_representation()}, 
             {{
                 "type": "tests.to_json.test_tarragon.Hook",
-                "id": {next(next_id_generator)},
+                "id": 1,
                 "object": {{
                     "ref": {{
-                        "ref_id": {current_id}
+                        "ref_id": 0
                     }}
                 }}
             }}
@@ -181,13 +170,58 @@ def test_set_to_json():
     assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
 
 
-def test_object_to_json():
+def test_set_to_json():
+    st = set()
+    hook = Hook(st)
+    st.add(hook)
+
+    actual_json = tarragon.to_json(st)
+    expected_json = f"""{{
+        "type": "set",
+        "id": 0,
+        "array": [
+            {{
+                "type": "tests.to_json.test_tarragon.Hook",
+                "id": 1,
+                "object": {{
+                    "ref": {{
+                        "ref_id": 0
+                    }}
+                }}
+            }}
+        ]
+    }}"""
+
+    assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
+
+
+def test_type_object_to_json():
     y = "1"
 
     actual_json = tarragon.to_json(Type1(y))
     expected_json = Type1Representation(y, next_id())()
 
     assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
+
+
+def test_complex_to_json():
+    obj = complex(1, 2)
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.complex", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(obj)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_obj: complex = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(actual_obj).is_equal_to(obj)
+        assertpy.assert_that(actual_obj.imag).is_equal_to(obj.imag)
+        assertpy.assert_that(actual_obj.real).is_equal_to(obj.real)
 
 
 def test_int_to_json():
@@ -229,3 +263,167 @@ def test_None_to_json():
     expected_json = "null"
 
     assertpy.assert_that(actual_json).is_equal_to(expected_json)
+
+
+def test_exception_to_json():
+    exc = Exception("test")
+
+    actual_json = tarragon.to_json(exc)
+    expected_json = f'''{{
+        "type": "builtins.Exception", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(exc)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_exc = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(actual_exc.args).is_equal_to(exc.args)
+
+
+def test_class_to_json():
+    raise NotImplementedError()
+
+
+def test_generator_to_json():
+    def get42():
+        yield 42
+
+    generator = get42()
+
+    assertpy.assert_that(tarragon.to_json).raises(TypeError).when_called_with(generator).is_equal_to(
+        "cannot pickle 'generator' object")
+
+
+def test_function_to_json():
+    def get42():
+        return 42
+
+    actual_json = tarragon.to_json(get42)
+    expected_json = f'''{{
+        "type": "builtins.function", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(get42)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_get42 = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(actual_get42()).is_equal_to(42)
+
+
+def test_iter_to_json():
+    lst = [1, 2, 3]
+    obj = iter(lst)
+    next(obj)
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.list_iterator", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(obj)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_obj = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(list(actual_obj)).is_equal_to([2, 3])
+        assertpy.assert_that(list(actual_obj)).is_empty()
+
+
+def test_range_to_json():
+    obj = range(3)
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.range", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(obj)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_obj = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(actual_obj).is_equal_to(range(3))
+        assertpy.assert_that(list(actual_obj)).is_equal_to([0, 1, 2])
+        assertpy.assert_that(list(actual_obj)).is_equal_to([0, 1, 2])
+
+
+def test_map_to_json():
+    obj = map(lambda x: x ** 2, [1, 2, 3])
+    next(obj)
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.map", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(obj)).decode("ascii")}"
+    }}'''
+
+    with assertpy.soft_assertions():
+        json_loads = json.loads(actual_json)
+        assertpy.assert_that(json_loads).is_equal_to(json.loads(expected_json))
+
+        actual_obj = dill.loads(base64.b64decode(json_loads["base64"].encode("ascii")))
+        assertpy.assert_that(list(actual_obj)).is_equal_to([4, 9])
+        assertpy.assert_that(list(actual_obj)).is_empty()
+
+
+def test_object_to_json():
+    obj = object()
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.object", 
+        "id": 0, 
+        "base64": "{base64.b64encode(dill.dumps(obj)).decode("ascii")}"
+    }}'''
+
+    assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
+
+
+def test_bytes_to_json():
+    obj = b"123"
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.bytes", 
+        "id": 0, 
+        "base64": "{base64.b64encode(obj).decode("ascii")}"
+    }}'''
+
+    assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
+
+
+def test_bytearray_to_json():
+    obj = bytearray(b"123")
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.bytearray", 
+        "id": 0, 
+        "base64": "{base64.b64encode(obj).decode("ascii")}"
+    }}'''
+
+    assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
+
+
+def test_memoryview_to_json():
+    obj = memoryview(b"123")
+
+    actual_json = tarragon.to_json(obj)
+    expected_json = f'''{{
+        "type": "builtins.memoryview", 
+        "id": 0, 
+        "base64": "{base64.b64encode(obj).decode("ascii")}"
+    }}'''
+
+    assertpy.assert_that(json.loads(actual_json)).is_equal_to(json.loads(expected_json))
